936dbfd6774290dd3cb4da632e893acabd86b602
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-05-11 06:06:14 -0300

    change axios base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'https://ainsight.vercel.app/',
+  baseURL:
+    process.env.NEXT_PUBLIC_BASE_URL || 'https://ainsight.vercel.app/api/',
 })
